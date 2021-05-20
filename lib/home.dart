b4afaf72212fa934a72ba521d8744bbd2732ee9e
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-05-20 23:26:41 +0200

    Add intl to format date

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
 import './models/transaction.dart';
+import './widgets/chart.dart';
+import './widgets/transaction_card.dart';
 
 class MyApp extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-      id: '1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
+        id: '1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(
-      id: '2',
-      title: 'New Shirt',
-      amount: 49.99,
-      date: DateTime.now(),
-    ),
+        id: '2', title: 'New Shirt', amount: 49.99, date: DateTime.now()),
   ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,49 +28,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Chart'),
-                ),
-                elevation: 3,
-              ),
-            ),
+            Chart(),
             ...transactions.map((index) {
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 8),
-                child: Card(
-                  child: ListTile(
-                    leading: Container(
-                      padding: EdgeInsets.all(6),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 3,
-                        ),
-                      ),
-                      child: Text(
-                        '${index.amount}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                    title: Text(
-                      '${index.title}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    subtitle: Text('${index.date}'),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
+              return TransactionCard(
+                title: index.title,
+                date: index.date,
+                amount: index.amount,
               );
             }).toList(),
           ],
